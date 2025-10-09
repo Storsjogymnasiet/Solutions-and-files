e4c998638539700abdf291ed3a07006d419bb4de
Deltidstomten <48528099+Deltidstomten@users.noreply.github.com>
2025-10-09 12:33:36 +0200

    Add files via upload

---
+++ inventory.py
@@ -0,0 +1,33 @@
+
+
+backpack = []
+
+while True:
+    print("Vad vill du göra?")
+    print("1. Visa Ryggsäcken")
+    print("2. Plocka upp något")
+    print("3. Släppa något i ryggsäcken")
+    print("4. Byta plats i ryggsäcken")
+    print("q. Stänga av")
+
+    choice = input()
+
+    if choice == "1":
+        pass
+        # din kod här
+    
+    elif choice == "2":
+        pass
+        # din kod här
+    elif choice == "3":
+        pass
+        # din kod här
+    elif choice == "4":
+        pass
+        # din kod här
+    elif choice == "q":
+        break
+    else:
+        print("Ogiltigt Svar")
+    
+    input("Tryck enter för att fortsätta")
