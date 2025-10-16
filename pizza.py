1715f771df0ecdd52121e068fdbcffb98b44ff04
Deltidstomten <elias.sjoling.njord@gmail.com>
2025-10-16 12:46:30 +0200

    asd

---
+++ pizza.py
@@ -0,0 +1,47 @@
+
+BASE_PRICE = 70
+
+pizza = []
+
+# Fyll på med flera ingredienser
+ingredients = [
+    ("Tomatsås", 20),
+    ("Ost", 25),
+    ("Skinka", 15)
+]
+
+
+print("Välkommen till pizzabagaren 3000")
+while True:
+    print("Vad vill du göra?")
+    print("1. Visa Möjliga Ingredienser")
+    print("2. Lägga till en ingrediens")
+    print("3. Ta bort en ingrediens")
+    print("4. Visa Pizza")
+    print("5. Räkna ut pris")
+    print("q. Stänga av")
+
+    choice = input()
+
+    if choice == "1":
+        pass
+        # din kod här
+    elif choice == "2":
+        pass
+        # din kod här
+    elif choice == "3":
+        pass
+        # din kod här
+    elif choice == "4":
+        pass
+        # din kod här
+    elif choice == "5":
+        pass
+        # din kod här
+    elif choice == "q":
+        break
+    else:
+        print("Ogiltigt Svar")
+    
+
+    input("\nTryck enter för att fortsätta...")
