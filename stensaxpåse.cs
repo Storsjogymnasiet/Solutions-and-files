7110cd5f07a7319130443d932b8cb21c7f75ec7e
Deltidstomten <elias.sjoling.njord@gmail.com>
2025-10-24 09:38:40 +0200

    asd

---
+++ stensaxpåse.cs
@@ -0,0 +1,11 @@
+Dictionary<string, string> winning_moves = new Dictionary<string, string>()
+        {
+            {"sten", "sax"},
+            {"sax", "påse"},
+            {"påse", "sten"}
+        };
+
+if (winning_moves.ContainsKey("sten") && winning_moves["sten"] == "sax")
+{
+    
+}
