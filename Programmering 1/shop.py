54a9e05c8ea05014b5b54be04b20e12fba615389
Deltidstomten <elias.sjoling.njord@gmail.com>
2025-11-20 09:05:35 +0100

    shop

---
+++ Programmering 1/shop.py
@@ -0,0 +1,47 @@
+shop = {
+
+}
+
+sales = {
+
+}
+
+print("Välkommen till Butikshanteraren 3000")
+print('Inloggad som Butiksägare')
+while True:
+    print("Vad vill du göra?")
+    print("1. Visa alla varor")
+    print("2. Lägg till en ny vara")
+    print("3. Ändra priset på vara")
+    print("4. Fylla på vara")
+    print("5. Sälja en vara")
+    print("6. Visa total intäkter")
+    print("q. Stänga av")
+
+    choice = input()
+
+    if choice == "1":
+        pass
+        # din kod här
+    
+    elif choice == "2":
+        pass
+        # din kod här
+    elif choice == "3":
+        pass
+        # din kod här
+    elif choice == "4":
+        pass
+        # din kod här
+    elif choice == "5":
+        pass
+        # din kod här
+    elif choice == "6":
+        pass
+        # din kod här
+    elif choice == "q":
+        break
+    else:
+        print("Ogiltigt Svar")
+    
+    input("\nTryck enter för att fortsätta...")
