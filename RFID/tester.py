0d1d8e01211a165cfd9e9eaeba8899fcde8f8810
Deltidstomten <48528099+Deltidstomten@users.noreply.github.com>
2026-03-13 14:45:14 +0100

    Update tester.py

--- RFID/tester.py
+++ RFID/tester.py
@@ -1,7 +1,13 @@
 from time import sleep
 from mfrc522 import SimpleMFRC522
+import socket
 import RPi.GPIO as GPIO
 reader = SimpleMFRC522()
+
+RECEIVER_IP = "10.0.100.146"  
+PORT = 6767
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
     # Loopar tills användaren gör ctrl + c
@@ -10,6 +16,7 @@ try:
         id, text = reader.read() # Väntar tills en tagg / kort läses
         print(f"ID: {id}")
         print(f"Text: {text}")
+        sock.sendto(str(id).encode("utf-8"), (RECEIVER_IP, PORT))
         sleep(2)
 except KeyboardInterrupt:
     GPIO.cleanup()
